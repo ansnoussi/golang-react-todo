b88b73680ff5741bfa7720c42e26a5b79dc0a3d1
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-10-12 00:10:08 +0200

    added some icons

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -1,13 +1,38 @@
 import React from "react";
+import { BsCheckLg, BsXLg, BsArrowCounterclockwise } from "react-icons/bs";
 
 function TodoItem({ id, item, onCheck }) {
   return (
-    <li
-      onClick={() => {
-        onCheck(id);
-      }}
-    >
-      {item}
+    <li>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        {item}
+        <div>
+          <BsCheckLg
+            style={{ color: "#2a9d8f", marginLeft: 10 }}
+            onClick={() => {
+              onCheck(id);
+            }}
+          />
+          <BsXLg
+            style={{ color: "#e63946", marginLeft: 10 }}
+            onClick={() => {
+              onCheck(id);
+            }}
+          />
+          <BsArrowCounterclockwise
+            style={{ color: "#ffb703", marginLeft: 10 }}
+            onClick={() => {
+              onCheck(id);
+            }}
+          />
+        </div>
+      </div>
     </li>
   );
 }
